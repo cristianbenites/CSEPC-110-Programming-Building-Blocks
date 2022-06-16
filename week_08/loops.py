b3ba27114180450750ff01d053cf17e1ef1347f3
cristianbenites <cristian.jbenites@gmail.com>
2022-06-15 23:44:56 -0400

    Adds milestone assignment of week 8

---
+++ week_08/loops.py
@@ -0,0 +1,12 @@
+
+colors = ["red", "blue", "green", "yellow"]
+
+for word in colors:
+    print(word)
+
+for number in range(1,9):
+    print(number)
+
+for number in range(2, 21, 2):
+    print(number)
+
